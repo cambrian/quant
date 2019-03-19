1f7af6908cafd126cd01792ca6c17412b489fbb2
Eddie Wang <eddie@vest.io>
2019-03-19 15:49:14 -0700

    output return on absolute positions as well as RoR

--- research/strategy.py
+++ research/strategy.py
@@ -18,6 +18,7 @@ class MovingAverage:
     def __init__(self, half_life):
         self.__a = 0.5 ** (1 / half_life)
         self.__value = None
+        self.__samples_needed = half_life
 
     @property
     def value(self):
@@ -27,3 +28,8 @@ class MovingAverage:
         if self.__value is None:
             self.__value = value
         self.__value = self.__a * self.__value + (1-self.__a) * value
+        self.__samples_needed = max(0, self.__samples_needed - 1)
+
+    @property
+    def ready(self):
+        return self.__samples_needed == 0
