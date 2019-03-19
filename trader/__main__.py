2af1116f753417a6932162bab2c988917d868076
Lucas Ege <lucas@vest.io>
2019-03-19 12:00:19 -0700

    Adding simple kraken WS - need async next

--- trader/__main__.py
+++ trader/__main__.py
@@ -1,9 +1,11 @@
 from exchange import Exchange
 from executor import Executor
 from strategy import Strategy
+from websocket import create_connection
 
 import asyncio
 import time
+import json
 
 
 class DummyExchange(Exchange):
@@ -13,6 +15,38 @@ class DummyExchange(Exchange):
             time.sleep(1)
             yield counter
             counter += 1
+
+
+class Kraken(Exchange):
+    # TODO: Update to be async ?
+    def _feed(self, pairs, time_interval):
+        for _ in range(3):
+            try:
+                self.ws = create_connection("wss://ws-sandbox.kraken.com")
+            except Exception as error:
+                print('Caught this error: ' + repr(error))
+                time.sleep(3)
+            else:
+                break
+        self.ws.send(json.dumps({
+            "event": "subscribe",
+            "pair": pairs,
+            "subscription": {
+                "name": "ohlc",
+                "interval": time_interval
+            }
+        }))
+
+        while True:
+            try:
+                result = self.ws.recv()
+                result = json.loads(result)
+                # Ignore heartbeats
+                if not isinstance(result, dict):
+                    yield result
+            except Exception as error:
+                print('Caught this error: ' + repr(error))
+                time.sleep(3)
 
 
 class DummyExecutor(Executor):
@@ -25,9 +59,10 @@ class DummyStrategy(Strategy):
         return str(data)
 
 
-exchange = DummyExchange()
+# exchange = DummyExchange()
+exchange = Kraken()
 strategy = DummyStrategy()
 executor = DummyExecutor()
-exchange_feed = exchange.observe([], 0)
+exchange_feed = exchange.observe(["XBT/USD"], 5)
 strategy_feed = strategy.process(exchange_feed)
 asyncio.run(executor.consume(strategy_feed))
