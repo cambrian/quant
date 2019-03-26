79347e36ee2608c6bb1f90f1c26e51828351d75f
Sanjay Kannan <whaatt@gmail.com>
2019-03-25 17:37:47 -0700

    more informative comments in __main__

--- live/__main__.py
+++ live/__main__.py
@@ -4,23 +4,26 @@ from trader.util.thread import ThreadManager
 
 import trader.executor as executor
 import trader.strategy as strategy
-
-# Get a thread manager and singletons.
 thread_manager = ThreadManager()
+
+# Initialize exchange singletons. Note that Bitfinex (and other exchanges in the future) require
+# you to put API keys in specific environment variables.
 bitfinex = Exchanges.get(BITFINEX)
 
 # Declare a new feed from Bitfinex price data (this only wires it up).
 bitfinex_btc_usd_1m = bitfinex.feed(thread_manager, 'BTCUSD', '1m')
 
-# Initialize and wire up our Dummy strategy.
+# Initialize and wire up our Dummy strategy. Strategies require particular feeds to be available to
+# them, and this is enforced on startup when things are being wired together.
 dummy_strategy = strategy.Dummy(thread_manager, {
     (BITFINEX, 'BTCUSD', '1m'): bitfinex_btc_usd_1m
 })
 
-# Initialize and wire up our Dummy executor.
+# Initialize and wire up our Dummy executor. In a similar vein, executors require particular
+# strategies to be available to them; this is also checked on startup.
 dummy_executor = executor.Dummy(thread_manager, {
     strategy.Dummy: dummy_strategy.feed
 })
 
-# Run feed processors.
+# Run components (forever).
 thread_manager.run()
