bc9d46a26201c5e3e753828901ebc3012a40ebb4
Sanjay Kannan <whaatt@gmail.com>
2019-03-25 17:44:35 -0700

    check

--- live/trader/executor/dummy.py
+++ live/trader/executor/dummy.py
@@ -16,20 +16,22 @@ class Dummy(Executor):
         }
 
     def _tick(self, strategy_inputs):
-        bitfinex = Exchanges.get(BITFINEX)
-        data = strategy_inputs[strategy.Dummy][BITFINEX]['BTCUSD']
-        ohlcv, fair_price, std_dev = itemgetter(
-            'ohlcv', 'fair_price', 'std_dev')(data)
-        close = float(ohlcv['close'])
-        print('Close: {}, Fair: {}, Std: {}'.format(
-            close, fair_price, std_dev))
-        if close < fair_price - std_dev:
-            print('Buying 0.0001 BTC at {}.'.format(close))
-            # print(bitfinex.add_order('BTCUSD', 'buy',
-            #                          'exchange market', ohlcv['close'], '0.004'))
-        elif close > fair_price + std_dev:
-            print('Selling 0.0001 BTC at {}.'.format(close))
-            # print(bitfinex.add_order('BTCUSD', 'sell',
-            #                          'exchange market', ohlcv['close'], '0.004'))
-        print(bitfinex.get_balance())
-        print(bitfinex.get_open_positions())
+        dummy_inputs = strategy_inputs[strategy.Dummy]
+        if BITFINEX in dummy_inputs and 'BTCUSD' in dummy_inputs[BITFINEX]:
+            bitfinex = Exchanges.get(BITFINEX)
+            data = dummy_inputs[BITFINEX]['BTCUSD']
+            ohlcv, fair_price, std_dev = itemgetter(
+                'ohlcv', 'fair_price', 'std_dev')(data)
+            close = float(ohlcv['close'])
+            print('Close: {}, Fair: {}, Std: {}'.format(
+                close, fair_price, std_dev))
+            if close < fair_price - std_dev:
+                print('Buying 0.0001 BTC at {}.'.format(close))
+                # print(bitfinex.add_order('BTCUSD', 'buy',
+                #                          'exchange market', ohlcv['close'], '0.004'))
+            elif close > fair_price + std_dev:
+                print('Selling 0.0001 BTC at {}.'.format(close))
+                # print(bitfinex.add_order('BTCUSD', 'sell',
+                #                          'exchange market', ohlcv['close'], '0.004'))
+            print(bitfinex.get_balance())
+            print(bitfinex.get_open_positions())
