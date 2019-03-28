cc541af2eee944bdd989c725e14b4d2840ab76ce
Sanjay Kannan <whaatt@gmail.com>
2019-03-28 12:38:50 -0700

    add individual currency constants

--- trader/util/constants.py
+++ trader/util/constants.py
@@ -8,16 +8,27 @@ Constants to identify common entities in the code.
 BITFINEX = "bitfinex"
 KRAKEN = "kraken"
 
+# Currencies.
+USD = "USD"
+USDT = "USDT"
+BTC = "BTC"
+ETH = "ETH"
+XRP = "XRP"
+
 # USD quotes.
-BTC_USD = ("BTC", "USD")
-ETH_USD = ("ETH", "USD")
-XRP_USD = ("XRP", "USD")
+BTC_USD = (BTC, USD)
+ETH_USD = (ETH, USD)
+XRP_USD = (XRP, USD)
 
 # USDT quotes.
-BTC_USDT = ("BTC", "USDT")
-ETH_USDT = ("ETH", "USDT")
-XRP_USDT = ("XRP", "USDT")
+BTC_USDT = (BTC, USDT)
+ETH_USDT = (ETH, USDT)
+XRP_USDT = (XRP, USDT)
 
 
 def base_currency(pair):
     return pair[0]
+
+
+def quote_currency(pair):
+    return pair[1]
