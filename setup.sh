a4a633ef8caa2bb9b8f21d07c77cb7f5430b259e
Pablo Aldape <pablo@vest.io>
2019-03-29 11:52:39 -0700

    exchange data dl

--- setup.sh
+++ setup.sh
@@ -7,6 +7,11 @@ else
     pip install -r requirements.txt
 fi
 
+# Grab test exchange data from S3
+python3 -m pip install awscli --upgrade --user
+mkdir -p research/data/test
+aws s3 cp --recursive s3://cambrianexchangedata/data/test/ research/data/test
+
 git config filter.nbstripout.clean 'nbstripout'
 git config filter.nbstripout.smudge cat
 git config filter.nbstripout.required true
