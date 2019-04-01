3777eb3312a21ce3f13b95ed52535d11f08dd842
Noah Golub <ngolub@stanford.edu>
2019-04-01 14:37:21 -0700

    Added good code.

---
+++ good.py
@@ -0,0 +1,12 @@
+def buy_low():
+    return "1 million stocks bought"
+
+def sell_high():
+    return "1 million dollars made"
+
+def main():
+    while (Vest.isSolventI()):
+        buy_low()
+        sell_high()
+
+
