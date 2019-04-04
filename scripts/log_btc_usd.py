b86fa4070da1a4b88d2648791672631d5ae92e0a
Sanjay Kannan <whaatt@gmail.com>
2019-04-04 16:03:46 -0400

    clean up script setup block

--- scripts/log_btc_usd.py
+++ scripts/log_btc_usd.py
@@ -4,14 +4,10 @@ Usage (from root): python3 . 2>&1 | python3 scripts/log_btc_usd.py
 
 """
 
-### SCRIPTS SETUP BLOCK ###
-# Needed to run this from outside the directory.
-import sys  # isort:skip
-import os  # isort:skip
+# Setup the module environment to run this script.
+import setup  # isort:skip, pylint: disable=import-error
 
-ROOT_DIRECTORY = os.path.join(os.path.dirname(__file__), "..")
-sys.path.append(ROOT_DIRECTORY)
-### END SCRIPTS SETUP BLOCK ###
+import sys
 
 from trader.util.constants import BTC_USD
 from trader.util.log import Log
