b545006f5008c47335c685c7cb0e9ad241f83c95
Lucas Ege <lucas@vest.io>
2019-04-05 10:14:20 -0700

    Adding *gross* support for single vs multiple pairs, looking for help on coint fixes

--- __main__.py
+++ __main__.py
@@ -4,7 +4,8 @@ import trader.strategy as strategy
 from trader.exchange import Bitfinex, DummyExchange
 from trader.executor import Executor
 from trader.metrics import Metrics
-from trader.util.constants import BITFINEX, BTC_USD, BTC_USDT, ETH_USD
+from trader.util.constants import (BITFINEX, BTC_USD, BTC_USDT, ETH_USD,
+                                   ETH_USDT)
 from trader.util.feed import Feed
 from trader.util.log import Log
 from trader.util.stats import Gaussian
@@ -12,15 +13,16 @@ from trader.util.thread import Beat, ThreadManager
 from trader.util.types import Direction, Order
 
 thread_manager = ThreadManager()
-bitfinex = Bitfinex(thread_manager)
+# bitfinex = Bitfinex(thread_manager)
 # If you don't have research/data/1min.h5, download 1min from S3 and run hd5.ipynb:
-# data_min = pd.read_hdf("research/data/1min.h5")
-# dummy_exchange = DummyExchange(thread_manager, data_min, {})
+data_min = pd.read_hdf("research/data/1min.h5")
+dummy_exchange = DummyExchange(thread_manager, data_min, {})
 
-dummy_strategy = strategy.Dummy()
-executor = Executor(thread_manager, {bitfinex: [BTC_USD, ETH_USD]})
-# executor = Executor(thread_manager, {dummy_exchange: [BTC_USDT]})
-metrics = Metrics(thread_manager, {bitfinex})
+# dummy_strategy = strategy.Dummy()
+cointegrator_strategy = strategy.Cointegrator(3)
+# executor = Executor(thread_manager, {bitfinex: [BTC_USD, ETH_USD]})
+executor = Executor(thread_manager, {dummy_exchange: [BTC_USDT]})
+# metrics = Metrics(thread_manager, {bitfinex})
 
 
 def main():
@@ -32,16 +34,15 @@ def main():
         executor.tick_fairs(fairs)
 
 
-# def dummy_main():
-#     beat = Beat(60000)
-#     while beat.loop():
-#         dummy_exchange.step_time()
-#         dummy_data = dummy_exchange.prices([BTC_USDT], "1m")
-#         dummy_fairs = dummy_strategy.tick(dummy_data)
-#         fairs = Gaussian.intersect([dummy_fairs])
-#         executor.tick_fairs(fairs)
+def dummy_main():
+    beat = Beat(10000)
+    while beat.loop():
+        dummy_exchange.step_time()
+        dummy_data = dummy_exchange.prices([BTC_USDT, ETH_USDT], "1m")
+        cointegration_fairs = cointegrator_strategy.step(dummy_data)
+        executor.tick_fairs(cointegration_fairs)
 
 
-thread_manager.attach("main", main)
-# thread_manager.attach("dummy_main", dummy_main, should_terminate=True)
+# thread_manager.attach("main", main)
+thread_manager.attach("dummy_main", dummy_main, should_terminate=True)
 thread_manager.run()
