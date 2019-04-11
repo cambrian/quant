c999466833a7dc6d8fe74ec663ad810b42ae536d
Pablo Aldape <pablo@vest.io>
2019-04-11 12:24:22 -0700

    postgres credentials

---
+++ pg_credentials.py
@@ -0,0 +1,7 @@
+# NOTE: All strings must be percent-encoded for use in URI construction. See urllib.parse.quote_plus
+
+PG_HOST = "exchange-data.cw4d8qrb7xlr.us-west-1.rds.amazonaws.com"
+PG_PORT = "5432"
+PG_DBNAME = "exchange_data"
+PG_USERNAME = "cambrian"
+PG_PASSWORD = "*tZ4s^K^8g37^V7YC9W42gg22ZZ*r;"
