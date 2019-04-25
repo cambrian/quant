1b857b05bc7130ddc3205d85cca3d2538510a7dd
Eddie Wang <eddie@vest.io>
2019-04-25 16:01:22 -0400

    .

--- __main__.py
+++ __main__.py
@@ -4,8 +4,7 @@ import trader.strategy as strategy
 from trader.exchange import Bitfinex, DummyExchange
 from trader.executor import Executor
 from trader.metrics import Metrics
-from trader.util.constants import (BITFINEX, BTC_USD, BTC_USDT, ETH_USD,
-                                   ETH_USDT)
+from trader.util.constants import BITFINEX, BTC_USD, BTC_USDT, ETH_USD, ETH_USDT
 from trader.util.feed import Feed
 from trader.util.log import Log
 from trader.util.stats import Gaussian
@@ -19,7 +18,7 @@ data_min = pd.read_hdf("research/data/1min.h5")
 dummy_exchange = DummyExchange(thread_manager, data_min, {})
 
 # dummy_strategy = strategy.Dummy()
-cointegrator_strategy = strategy.Cointegrator(6000, 30)
+cointegrator_strategy = strategy.Cointegrator(4000, 2000, 100, 30)
 kalman_strategy = strategy.Kalman(512, 100, 32, 8)
 # executor = Executor(thread_manager, {bitfinex: [BTC_USD, ETH_USD]})
 executor = Executor(thread_manager, {dummy_exchange: [BTC_USDT, ETH_USDT]}, size=100, min_edge=0)
