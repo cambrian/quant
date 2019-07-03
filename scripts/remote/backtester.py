b6a8c63f6f2c1a317d579295164185c6d9a0e65d
Lucas Ege <lucas@vest.io>
2019-07-03 14:28:26 -0700

    Adding backtesting runner, will move later

---
+++ scripts/remote/backtester.py
@@ -0,0 +1,10 @@
+def job(sc, input_path, working_dir):
+    """Your entire job must go within the function definition (including imports)."""
+    from backtest import backtest_spark_job, analyze_spark_job
+
+    results = backtest_spark_job("research/data/1min.h5", sc)[0]
+    price_data = results["data"].apply(lambda x: x["price"])
+    for row in price_data.columns:
+        if row.base not in results["balances"]:
+            results["balances"][row.base] = 0.0
+    return analyze_spark_job(sc, results)
