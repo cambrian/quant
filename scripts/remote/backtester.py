e2cc3e7babd5aea66987990fb7fa7acb6b0dedb9
Lucas Ege <lucas@vest.io>
2019-07-05 18:37:16 -0700

    Updating backtester to multilevel indexing, still needs emr bugs to be fixed

--- scripts/remote/backtester.py
+++ scripts/remote/backtester.py
@@ -3,7 +3,7 @@ def job(sc, input_path, working_dir):
     from backtest import backtest_spark_job, analyze_spark_job
 
     results = backtest_spark_job("research/data/1min.h5", sc)[0]
-    price_data = results["data"].apply(lambda x: x["price"])
+    price_data = results["data"].xs("price", axis=1, level=1)
     for row in price_data.columns:
         if row.base not in results["balances"]:
             results["balances"][row.base] = 0.0
